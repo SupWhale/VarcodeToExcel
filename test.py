686ea204669de9f27416c5a075500fb52fd16b6b
sharkwhale <kmjbs313@gmail.com>
2021-04-05 16:00:03 +0900

    간단한 gui 추가 버전

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
+from PIL import Image
+from PIL import ImageTk
+import keyboard
+import tkinter as tk
+import threading
+import datetime
+import os
 import cv2
 import pyzbar.pyzbar as pyzbar
 import numpy as np
@@ -24,12 +31,10 @@ def macadd(mac):
         result += j
     return result
 
-    
-
-if __name__ == '__main__': #Main
+def camThread():
     cap = cv2.VideoCapture(1) #외부 웹캠을 불러옴
     write_wb = Workbook() #워크시트를 생성할 객체 생성
-
+    panel = None
     write_ws = write_wb.create_sheet('생성시트') #워크 시트 생성
     
     print('width :%d, height : %d' % (cap.get(3), cap.get(4))) #현재 카메라의 해상도 출력
@@ -40,10 +45,23 @@ if __name__ == '__main__': #Main
     while(True):
         ret, frame = cap.read()    # Read 결과와 frame
         if(ret) :
-            cv2.imshow('frame_color', frame) # 컬러 화면 출력
-            if cv2.waitKey(1) == ord('q'): #q버튼 누를시 이미지 촬영
-                cv2.imwrite('bacode.jpg', frame, params=[cv2.IMWRITE_PNG_COMPRESSION,0]) #이미지파일 출력
-                im = cv2.imread('bacode.jpg')
+            
+           #cv2.imshow('frame_color', frame) # 컬러 화면 출력
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image = Image.fromarray(image)
+            image = ImageTk.PhotoImage(image)
+
+            if panel is None:
+                panel = tk.Label(image=image)
+                panel.image = image
+                panel.pack(side="left")
+            else:
+                panel.configure(image=image)
+                panel.image = image
+            #if cv2.waitKey(1) == ord('q'): #q버튼 누를시 이미지 촬영
+            if keyboard.is_pressed('q'):
+                cv2.imwrite(os.path.join(os.path.abspath('jpg') , 'bacode.jpg'), frame, params=[cv2.IMWRITE_PNG_COMPRESSION,0]) #이미지파일 출력
+                im = cv2.imread(os.path.join(os.path.abspath('jpg') , 'bacode.jpg'))
                     
                 decodedObjects = decode(im) #바코드 해석
                 write_ws = write_wb.active #엑셀 파일에 쓰기 활성화
@@ -74,7 +92,7 @@ if __name__ == '__main__': #Main
                         b = b
                         print('인식 불가')
                     
-                write_wb.save("test3.xlsx") #엑셀 파일 저장
+                write_wb.save(os.path.join(os.path.abspath('xlsx') , 'test.xlsx')) #엑셀 파일 저장
 
             elif cv2.waitKey(1) == ord('w'): #w를 누룬다면 프로그램 종료
                 break
@@ -84,6 +102,21 @@ if __name__ == '__main__': #Main
     cap.release() #프로그램 최종종료
     cv2.destroyAllWindows()
 
+
+
+
+
+
+if __name__ == '__main__': #Main
+    thread_img = threading.Thread(target=camThread, args=())
+    thread_img.daemon = True
+    thread_img.start()
+
+    root = tk.Tk()
+    root.mainloop()
+
+    
+    
 
 
     
