56bc2be46218814e8252148021f98c82ebbea765
sharkwhale <kmjbs313@gmail.com>
2021-06-02 12:56:21 +0900

    재고 관리 모드 기능 개선, 기타 오류 개선

--- test.py
+++ test.py
@@ -13,14 +13,27 @@ from datetime import datetime
 
 
 
+def decode_serial(im): #이미지내에서 바코드를 찾아내고 해당 타입과 데이터를 출력하는 함수
+    
+    decodedObjects = pyzbar.decode(im)
+
+    result = ""
+    for obj in decodedObjects:
+        print('Type : ', obj.type)
+        print('Data : ', obj.data, '\n')
+        result = obj.data
+        print(result[0])
+    return result
+
 def decode(im): #이미지내에서 바코드를 찾아내고 해당 타입과 데이터를 출력하는 함수
     
     decodedObjects = pyzbar.decode(im)
-    
+
     for obj in decodedObjects:
         print('Type : ', obj.type)
         print('Data : ', obj.data, '\n')
     return decodedObjects
+
 
 def macadd(mac):
     
@@ -43,17 +56,21 @@ def macadd(mac):
     return result
 
 def CheckType_Serial(code):
+    try:   
+        result = ""
+        if code[0] == 84:
+            result = "TMS"
+        elif code[0] == 77:
+            result = "PMC"
+        elif code[0] == 71:
+            result = "GateWay"
+        elif code[0] == 82:
+            result = "IRC"
+        else:
+            result = ""
+    except IndexError as e:
+        print(e)
     
-    result = ""
-    if code[0] == 'T':
-        result = "Tem"
-    elif code[0] == 'M':
-        result = "Pow"
-    elif code[0] == 'G':
-        result = "GateWay"
-    elif code[0] == "R":
-        result = "IR"
-
     return result
         
 def Setmode_Doc(ch):
@@ -70,23 +87,54 @@ def Setmode_Install(ch):
     global choice
     State.config(text = '설치정보모드')
     choice = ch + 3
+
+def Doctor(write_ws, decodedObjects,width,high): #의사협회 관리 모드
+    
+    for obj in decodedObjects:
+        code = obj.data
+        result.config(text = obj.data)
+        write_ws.cell(high,width,macadd(code.decode()))     
+       
+def Stock_Manage(write_ws, decodedObjects,width,high): #재고 관리 모드
+
+        result.config(text = decodedObjects)
+        write_ws.cell(high,width,decodedObjects)
+
+def install_Int(write_ws, decodedObjects,width,high): #GEM 설치정보 양식에 맞춘 모드
+    
+    for obj in decodedObjects:
+        code = obj.data
+        result.config(text = obj.data)
+        str_high = str(high)
+        location = 'A'+str_high
+        write_ws[location] = obj.data
+        merge_location = 'A'+str_high+':C'+str_high
+        write_ws.merge_cells(merge_location)
     
 def camThread():
+    
     cap = cv2.VideoCapture(0) #외부 웹캠을 불러옴
     write_wb = Workbook() #워크시트를 생성할 객체 생성
     panel = None #GUI를 생성할 판넬 생성
-    write_ws = write_wb.create_sheet('생성시트') #워크 시트 생성
+    write_ws = write_wb.create_sheet('기본시트') #워크 시트 생성
+    gateway_ws = write_wb.create_sheet('Gateway')
+    pmc_ws = write_wb.create_sheet('PMC')
+    tms_ws = write_wb.create_sheet('TMS')
+    irc_ws = write_wb.create_sheet('IRC')
     
     print('width :%d, height : %d' % (cap.get(3), cap.get(4))) #현재 카메라의 해상도 출력
 
     width = 2
     high = 3
+    pmc_h=1
+    irc_h=1
+    tms_h=1
+    gateway_h=1
     check_code = False
     while(True):
         ret, frame = cap.read()    # Read 결과와 frame
         if(ret) :
             
-           #cv2.imshow('frame_color', frame) # 컬러 화면 출력
             image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             image = Image.fromarray(image)
             image = ImageTk.PhotoImage(image)
@@ -105,57 +153,66 @@ def camThread():
                 cv2.imwrite('bacode.jpg', frame) #이미지파일 출력
                 im = cv2.imread('bacode.jpg')
                     
-                decodedObjects = decode(im) #바코드 해석
-                write_ws = write_wb.active #엑셀 파일에 쓰기 활성화            
+                decodedObjects = decode(im)#바코드 해석 
+                print(decodedObjects)
+              
+                if decodedObjects == '[]':
+                    print("감지된 바코드가 없습니다.")
+                    continue
                 
-                if choice == 1:
-                    Doctor(write_ws, decodedObjects,width,high)
-                   
-                    if width < 6:
-                        width = width+1
-                    elif width == 6:
-                        width = 3
+                else:
+                
+                    if choice == 1:
+                        
+                        Doctor(write_ws, decodedObjects,width,high)
+                       
+                        if width < 6:
+                            width = width+1
+                        elif width == 6:
+                            width = 3
+                            high = high+1
+                            
+                    elif choice == 2:
+                        
+                        decodedObjects_s = decode_serial(im)
+                        print(decodedObjects_s)
+
+                        types = CheckType_Serial(decodedObjects_s)
+                        
+                        if types == "TMS":
+                            
+                            #tms_ws = write_wb.active
+                            Stock_Manage(tms_ws, decodedObjects_s,width,tms_h)
+                            tms_h = tms_h+1
+                            
+                        elif types == "PMC":
+                            
+                            Stock_Manage(pmc_ws, decodedObjects_s,width,pmc_h)
+                            pmc_h = pmc_h + 1
+                            
+                        elif types == "IRC":
+
+                            Stock_Manage(irc_ws, decodedObjects_s,width,irc_h)
+                            irc_h = irc_h + 1
+                            
+                        elif types == "GateWay":
+                            
+                            Stock_Manage(gateway_ws, decodedObjects_s,width,gateway_h)
+                            gateway_h = gateway_h+1
+                            
                         high = high+1
                         
-                elif choice == 2:
-                    Stock_Manage(write_ws, decodedObjects,width,high)
-                    high = high+1
+                    elif choice == 3:
+                        
+                        install_Int(write_ws, decodedObjects,width,high)
+                        high = high+1
                     
-                elif choice == 3:
-                    install_Int(write_ws, decodedObjects,width,high)
-                    high = high+1
+                    str = datetime.today().strftime("%Y년%m월%d일%H시")
+                        
+                    write_wb.save(str + '.xlsx') #엑셀 파일 저장
                 
-                str = datetime.today().strftime("%Y년%m월%d일%H시")
-                    
-                write_wb.save(str + '.xlsx') #엑셀 파일 저장
-            
     cap.release() #프로그램 최종종료
     cv2.destroyAllWindows()
-
-def Doctor(write_ws, decodedObjects,width,high):
-    
-    for obj in decodedObjects:
-        code = obj.data
-        result.config(text = obj.data)
-        write_ws.cell(high,width,macadd(code))       
-    
-                
-def Stock_Manage(write_ws, decodedObjects,width,high):
-
-    for obj in decodedObjects:
-        code = obj.data
-        result.config(text = obj.data)
-        write_ws.cell(high,width,obj.data)
-
-def install_Int(write_ws, decodedObjects,width,high):
-    for obj in decodedObjects:
-        code = obj.data
-        result.config(text = obj.data)
-        str_high = str(high)
-        location = 'A'+str_high
-        write_ws[location] = obj.data
-        merge_location = 'A'+str_high+':C'+str_high
-        write_ws.merge_cells(merge_location)
        
 if __name__ == '__main__': #Main
     thread_img = threading.Thread(target=camThread, args=())
